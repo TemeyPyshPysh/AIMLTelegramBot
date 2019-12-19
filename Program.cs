824c8437ab48da673906fd0666ed99076ac62d2d
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2019-12-19 22:12:38 +0300

    simple telegram bot added

--- Program.cs
+++ Program.cs
@@ -4,22 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AIMLbot;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using System.IO;
+using System.Net;
 
 namespace AIMLTelegramBot
 {
     class Program
     {
+        private static ITelegramBotClient botClient;
+        private static Bot AI;
+        private static User myUser;
+        private static string LoadKey(string pathToFile)
+        {
+            StreamReader sr = new StreamReader(pathToFile);
+            string key = sr.ReadLine().Replace(" ", string.Empty);
+
+            return key;
+        }
         static void Main(string[] args)
         {
-            Bot AI = new Bot();
+            AI = new Bot();
             AI.loadSettings();
             AI.loadAIMLFromFiles();
 
             AI.isAcceptingUserInput = false;
 
-            User myUser = new User("username", AI);
+            myUser = new User("username", AI);
             AI.isAcceptingUserInput = true;
 
+            string pathToFile = "config/bot_key";
+            string accessToken = LoadKey(pathToFile);
+
+            var proxy = new WebProxy("20.40.147.38:8080");
+
+            botClient = new TelegramBotClient(accessToken, proxy);
+            var me = botClient.GetMeAsync().Result;
+
+            Console.WriteLine(
+                $"I'M ALIVE 4AW3 1S {me.Id} :: {me.FirstName}"
+            );
             while (true)
             {
                 Request r = new Request(Console.ReadLine(), myUser, AI);
